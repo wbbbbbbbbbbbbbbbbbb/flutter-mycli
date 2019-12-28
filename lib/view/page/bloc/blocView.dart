82c65f533bed46445f22eedc319a7e219434cdf9
webbing <1158211734@qq.com>
2019-12-28 15:38:48 +0800

    新增bloc

--- lib/view/page/bloc/blocView.dart
+++ lib/view/page/bloc/blocView.dart
@@ -1,28 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/route/index.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc.dart';
 
 class BlocView extends StatelessWidget {
   const BlocView({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("BloC 教程"),
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            RaisedButton(
-              onPressed: () => Router.to("/bloc"),
-              child: Text("bloc 例子"),
+    return BlocProvider<CounterBloc>(
+      create: (BuildContext context) => CounterBloc(),
+      child: Builder(
+        builder: (BuildContext context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("BloC 教程"),
             ),
-            RaisedButton(
-              onPressed: () => Router.clearStackTo("/"),
-              child: Text("bloc 清栈"),
+            body: Scaffold(
+              floatingActionButton: FloatingActionButton(
+                onPressed: () {
+                  BlocProvider.of<CounterBloc>(context).add(CounterEvent.increment);
+                },
+                child: Text("+"),
+              ),
+              body: Center(
+                child: BlocBuilder<CounterBloc, int>(builder: (context, state) {
+                  return Text("$state");
+                }),
+              ),
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
